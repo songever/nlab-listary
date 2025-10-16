b84d177e5463e9cc21dd3690bb73d8bc2816a362
songever <1021940593@qq.com>
2025-10-16 15:17:49 +0800

    remove redundent files

--- src/lib.rs
+++
@@ -1,9 +0,0 @@
-pub const REPO_URL: &str = "https://github.com/ncatlab/nlab-content-html.git";
-pub const LOCAL_PATH: &str = "nlab_mirror";
-
-pub mod browser;
-pub mod git_ops;
-pub mod models;
-pub mod parser;
-pub mod search;
-pub mod storage;
